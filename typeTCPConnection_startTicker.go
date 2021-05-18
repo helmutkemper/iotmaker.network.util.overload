1378c3a5852c997a04a0c65afcd275e501046bb1
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-17 23:39:44 -0300

    documenting

--- typeTCPConnection_startTicker.go
+++ typeTCPConnection_startTicker.go
@@ -8,6 +8,7 @@ import (
 //
 // startTicker (Português):
 func (el *TCPConnection) startTicker() {
+	el.ticker.Stop()
 	el.ticker = time.NewTicker(el.delays.GenerateTime())
 	//el.ticker.Reset(el.delays.GenerateTime())
 }
