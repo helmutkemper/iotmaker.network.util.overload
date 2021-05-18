26a7f5b73aa0024a97f70c1ffa757b4092af50a1
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-17 22:08:50 -0300

    documenting

--- typeTCPConnection_startTicker.go
+++ typeTCPConnection_startTicker.go
@@ -1,8 +1,13 @@
 package iotmakernetworkutiloverload
 
+import (
+	"time"
+)
+
 // startTicker (English):
 //
 // startTicker (Português):
 func (el *TCPConnection) startTicker() {
-	el.ticker.Reset(el.delays.GenerateTime())
+	el.ticker = time.NewTicker(el.delays.GenerateTime())
+	//el.ticker.Reset(el.delays.GenerateTime())
 }
